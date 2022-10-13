7fa09983f4f937e65f4bb00619c0c18d63c49708
lrhzds <2659538504@qq.com>
2022-10-14 00:12:15 +0800

    commit

--- Class3/actionmodemenu/src/main/java/com/example/actionmodemenu/MainActivity.java
+++ Class3/actionmodemenu/src/main/java/com/example/actionmodemenu/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -20,26 +24,108 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BaseAdapter adapter;
     private String[] names = {"One", "Two", "Three", "Four", "Five"};
-    private int imageId = R.mipmap.ic_launcher_round;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<Map<String, Object>> list = new ArrayList<>();
+        List<Item> list = new ArrayList<>();
         for (int i = 0; i < names.length; i++) {
-            Map<String, Object> listItem = new HashMap<>();
-            listItem.put("names", names[i]);
-            listItem.put("pic", imageId);
-            list.add(listItem);
+            list.add(new Item(names[i], false));
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list, R.layout.simple_item, new String[]{"names", "pic"}, new int[]{R.id.name, R.id.pic});
+        adapter = new AdapterCur(list, MainActivity.this);
 
         ListView listView = findViewById(R.id.listView);
 
-        listView.setAdapter(simpleAdapter);
+        listView.setAdapter(adapter);
+
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+            int num = 0;
+
+            /*
+             * 参数：ActionMode是长按后出现的标题栏
+             * 		positon是当前选中的item的序号
+             *		id 是当前选中的item的id
+             *		checked 如果是选中事件则为true，如果是取消事件则为false
+             */
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                if (checked == true) {
+                    list.get(position).setBo(true);
+                    //实时刷新
+                    adapter.notifyDataSetChanged();
+                    num++;
+                } else {
+                    list.get(position).setBo(false);
+                    //实时刷新
+                    adapter.notifyDataSetChanged();
+                    num--;
+                }
+                // 用TextView显示
+                mode.setTitle("  " + num + " Selected");
+            }
+
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    //全选
+                    case R.id.selected:
+                        num = 0;
+                        refresh();
+                        adapter.notifyDataSetChanged();
+                        mode.finish();
+                        return true;
+                    //删除
+                    case R.id.delete:
+                        adapter.notifyDataSetChanged();
+                        num = 0;
+                        refresh();
+                        mode.finish();
+                        return true;
+                    default:
+                        refresh();
+                        adapter.notifyDataSetChanged();
+                        num = 0;
+                        return false;
+                }
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                // Inflate the menu for the CAB
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.action_menu, menu);
+                num = 0;
+                return true;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                refresh();
+                adapter.notifyDataSetChanged();
+            }
+            public void refresh(){
+                for(int i = 0; i < names.length; i++){
+                    list.get(i).setBo(false);
+                }
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
